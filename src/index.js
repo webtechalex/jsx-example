70ffb7b58411b26e4ebdae7a2f3d5b219f6e9a7a
alexanderbrown <alexanderbrown.info@gmail.com>
2024-03-29 10:00:53 +0000

    remove console log

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(root);
 root.render(
   <StrictMode>
     <App />
